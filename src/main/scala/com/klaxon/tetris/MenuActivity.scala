283e81c065c1af9f9fbbe21a3acb6074cbf31451
Volodymyr Pronyshyn <skatyg12@gmail.com>
2013-11-28 21:54:05 +0200

    As it figured out scala and android plugins doesn't go well on gradle. Also haven't succeed with same on sbt.
    Therefore here is our goodie maven with scala and android stuff. Should to say it works pretty well. I'm thankfull to the developers of android-maven-plugin.
    Initial working commit with scala in da house.

---
+++ src/main/scala/com/klaxon/tetris/MenuActivity.scala
@@ -0,0 +1,11 @@
+package com.klaxon.tetris
+
+import android.app.Activity
+import android.os.Bundle
+
+class MenuActivity extends Activity {
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+  }
+}
