639bc062b2e7863bb35479f716db7a3ee270a418
Volodymyr Pronyshyn <skatyg12@gmail.com>
2013-11-30 20:23:31 +0200

    Click listener on exit view added

--- src/main/scala/com/klaxon/tris/MenuActivity.scala
+++ src/main/scala/com/klaxon/tris/MenuActivity.scala
@@ -2,10 +2,16 @@ package com.klaxon.tris
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 
 class MenuActivity extends Activity {
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.main)
+    setContentView(R.layout.menu)
+
+    findViewById(R.id.exit).setOnClickListener(new OnClickListener{
+      def onClick(p1: View) = finish()
+    })
   }
 }
