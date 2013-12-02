378fdc4c6d0a2a29ca877d8454cc1cf6f872683e
Volodymyr Pronyshyn <skatyg12@gmail.com>
2013-12-02 23:12:35 +0200

    Initial commit for Classical version of game added

--- src/main/scala/com/klaxon/tris/MenuActivity.scala
+++ src/main/scala/com/klaxon/tris/MenuActivity.scala
@@ -4,14 +4,26 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
+import android.content.Intent
+import com.klaxon.tris.classical.ClassicalActivity
 
 class MenuActivity extends Activity {
+
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.menu)
 
-    findViewById(R.id.exit).setOnClickListener(new OnClickListener{
-      def onClick(p1: View) = finish()
-    })
+    findViewById(R.id.exit).setOnClickListener((v: View) => finish())
+    findViewById(R.id.play).setOnClickListener((v: View) => startClassicalGame)
   }
+
+  private def startClassicalGame = {
+    val intent = new Intent(this, classOf[ClassicalActivity])
+    startActivity(intent)
+  }
+
+  implicit def function2onClickListener(f: View => Unit):OnClickListener = new OnClickListener {
+    def onClick(v: View) = f(v)
+  }
+
 }
---
+++ src/main/scala/com/klaxon/tris/classical/ClassicalActivity.scala
@@ -0,0 +1,18 @@
+package com.klaxon.tris.classical
+
+import android.app.Activity
+import android.os.Bundle
+import com.klaxon.tris.R
+
+/**
+ * <p>User: v.pronyshyn<br/>
+ * Date: 12/2/13</p>
+ */
+class ClassicalActivity extends Activity {
+
+  override def onCreate(savedInstanceState: Bundle) = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.classical_layout)
+  }
+
+}
